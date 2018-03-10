cbf6e2ad144064466b6ac98adc0eb559a50fc957
Ankur Satya <satya.ankur@gmail.com>
2018-03-10 22:42:02 +0530

    Add files via upload

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -2,7 +2,7 @@
 #include "tools.h"
 #include "Eigen/Dense"
 #include <iostream>
-#include<cmath>
+#include <cmath>
 
 using namespace std;
 using Eigen::MatrixXd;
@@ -14,10 +14,10 @@ using std::vector;
  */
 FusionEKF::FusionEKF() {
   is_initialized_ = false;
-  state_velocity_init = 1;
+  // state_velocity_init = 1;
 
   previous_timestamp_ = 0;
-  deltaT=0;
+  deltaT=0.0;
 
   // initializing matrices
   R_laser_ = MatrixXd(2, 2);
@@ -66,23 +66,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1; // Random velocity given at starting.
-    
+    ekf_.x_ << 1, 1, 0, 0; // Random velocity given at starting.
+   
     ekf_.P_ = MatrixXd(4, 4);
-    ekf_.P_ <<   10, 0, 0, 0,
-                 0, 10, 0, 0,
-                 0, 0, 2.5, 0,
-                 0, 0, 0, 2.5;
+    ekf_.P_ <<   1, 0, 0, 0,
+                 0, 1, 0, 0,
+                 0, 0, 1, 0,
+                 0, 0, 0, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) 
     {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+      // tools.pi_range(measurement_pack.raw_measurements_);
       ekf_.x_ << measurement_pack.raw_measurements_[0] * cos(measurement_pack.raw_measurements_[1]), 
                  measurement_pack.raw_measurements_[0] * sin(measurement_pack.raw_measurements_[1]),
-                 measurement_pack.raw_measurements_[2] * cos(measurement_pack.raw_measurements_[1]),
-                 measurement_pack.raw_measurements_[2] * sin(measurement_pack.raw_measurements_[1]);
+                 0,
+                 0;
+                 // measurement_pack.raw_measurements_[2] * cos(measurement_pack.raw_measurements_[1]),
+                 // measurement_pack.raw_measurements_[2] * sin(measurement_pack.raw_measurements_[1]);
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
@@ -90,27 +93,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       */
       ekf_.x_[0] = measurement_pack.raw_measurements_[0];
       ekf_.x_[1] = measurement_pack.raw_measurements_[1];
-      state_velocity_init = 0;
+      ekf_.x_[2] = 0;
+      ekf_.x_[3] = 0;
+      // state_velocity_init = 0;
     }
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
     previous_timestamp_= measurement_pack.timestamp_;
+    ekf_.F_ = MatrixXd(4,4);
     ekf_.DT = MatrixXd(4,2);
+    ekf_.Q_ = MatrixXd(4,4);
+    // ekf_.DT = MatrixXd(4,2);
     return;
   }
 
-  if (!state_velocity_init && measurement_pack.raw_measurements_.size()==3)
-  { 
-       ekf_.x_[2] = measurement_pack.raw_measurements_[2] * cos(measurement_pack.raw_measurements_[1]),
-       ekf_.x_[3] = measurement_pack.raw_measurements_[2] * sin(measurement_pack.raw_measurements_[1]);
-       state_velocity_init = 1;
-  }
   /*****************************************************************************
    *  Prediction
    ****************************************************************************/
 
-  /**
+  /*
    TODO:
      * Update the state transition matrix F according to the new elapsed time.
       - Time is measured in seconds.
@@ -118,22 +120,30 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
-  deltaT = measurement_pack.timestamp_ - previous_timestamp_;
+  deltaT = (measurement_pack.timestamp_ - previous_timestamp_)/1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
 
-  ekf_.F_ = MatrixXd(4,4);
   ekf_.F_ << 1, 0, deltaT, 0,
              0, 1, 0, deltaT,
              0, 0, 1, 0,
              0, 0, 0, 1;
 
+  
   ekf_.DT << 0.5 * (deltaT*deltaT), 0,
              0, 0.5 * (deltaT*deltaT),
              deltaT, 0,
              0, deltaT;
 
-  ekf_.Q_ = MatrixXd(4,4);
-  ekf_.Q_ << (ekf_.DT * ekf_.u) * ((ekf_.DT * ekf_.u).transpose());
+  // float t_=deltaT*deltaT;
+  // float t_cube=t_*deltaT;
+  // float t_quad=t_*t_;
 
+  // ekf_.Q_ <<  t_quad*ekf_.u[0]/4, 0, t_cube*ekf_.u[0]/2,0,
+  //             0,t_quad*ekf_.u[1]/4,0,t_cube*ekf_.u[1]/2,
+  //             t_cube*ekf_.u[0]/2,0,t_*ekf_.u[0],0,
+  //             0,t_cube*ekf_.u[1]/2,0,t_*ekf_.u[1];
+
+  ekf_.Q_ << (ekf_.DT * ekf_.u) * ((ekf_.DT * ekf_.u).transpose());
   ekf_.Predict();
 
   /*****************************************************************************
@@ -148,19 +158,23 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-    Hj_ = tools.CalculateJacobian
+    // return;
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
-    UpdateEKF();
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } 
   else {
+    // cout<<"Laser"<<endl;
     // Laser updates
+    // return;
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
-    Update();
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
-  cout << "x_ = " << ekf_.x_ << endl;
-  cout << "P_ = " << ekf_.P_ << endl;
+  cout<<"Hj_ - "<<endl<<Hj_<<endl;
+  // cout << "x_ = " << ekf_.x_ << endl;
+  // cout << "P_ = " << ekf_.P_ << endl;
 }
